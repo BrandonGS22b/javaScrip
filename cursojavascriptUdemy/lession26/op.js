c487b6b152d242a7f5c7751d3b544afb856da2ab
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-13 16:22:24 -0500

    lession 26 y 27

---
+++ cursojavascriptUdemy/lession26/op.js
@@ -0,0 +1,37 @@
+let numero = 8;
+let numeroTexto = 'VALOR DESCONOCIDO';
+
+switch (numero) {
+    case 1:
+        numeroTexto = 'NÚMERO UNO';
+        break;
+    case 2:
+        numeroTexto = 'NÚMERO DOS';
+        break;
+    case 3:
+        numeroTexto = 'NÚMERO TRES';
+        break;
+    case 4:
+        numeroTexto = 'NÚMERO CUATRO';
+        break;
+    case 5:
+        numeroTexto = 'NÚMERO CINCO';
+        break;
+    case 6:
+        numeroTexto = 'NÚMERO SEIS';
+        break;
+    case 7:
+        numeroTexto = 'NÚMERO SIETE';
+        break;
+    case 8:
+        numeroTexto = 'NÚMERO OCHO';
+        break;
+    case 9:
+        numeroTexto = 'NÚMERO NUEVE';
+        break;
+    default:
+        numeroTexto = 'VALOR DESCONOCIDO';
+        break;
+}
+
+console.log(numeroTexto);
