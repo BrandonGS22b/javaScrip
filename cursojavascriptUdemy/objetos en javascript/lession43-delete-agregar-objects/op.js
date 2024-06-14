9d9f11365e1eb9b43a1bb9dc8fbca38db201c419
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-13 22:39:15 -0500

    eliminar objetos
    delete.persona.telefono

---
+++ cursojavascriptUdemy/objetos en javascript/lession43-delete-agregar-objects/op.js
@@ -0,0 +1,25 @@
+
+//agregar y eliminar propiedades de nuestros objetos
+let persona ={
+    nombre: "brandon",
+    apellido: "bueno",
+    
+    email:"brandon@gmail.com",
+    edad: 25,
+    //anadimos un metodo 
+    nombrecompleto : function(){
+        return this.nombre + " " + this.apellido;
+    }
+
+
+}
+
+persona.telefono = " 3214481541";
+persona.telefono= "322248454";
+
+
+//con esto elimino el telegono
+delete persona.telefono;
+delete persona.nombrecompleto;
+
+console.log(telefono);
