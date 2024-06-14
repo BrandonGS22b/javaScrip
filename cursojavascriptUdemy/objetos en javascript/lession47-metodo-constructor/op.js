9915b1347dc99e191dc687a87d2ed1a7cf68ed41
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-13 23:17:44 -0500

    se agrega metodo al contructor y lo llame en el cosole.log name completo

--- cursojavascriptUdemy/objetos en javascript/lession47-metodo-constructor/op.js
+++ cursojavascriptUdemy/objetos en javascript/lession47-metodo-constructor/op.js
@@ -7,14 +7,23 @@ this.apellido = apellido;
 
 this.email = email;
 
+//metodo lession48
+this.nombrecompleto= function(){ 
+
+
+    return this.nombre + " " + this.apellido;
+}
+
 
 }
 //objeto padre aqui agregamos los objetos
 let padre=  new Personas("brandon","garcia","brandon@hotmail.com");
 
-console.log(padre);
+//llamamos el nombre completo del padre que hizimos en el constructor
+console.log(padre.nombrecompleto());
 
 
 let madre=  new Personas("emilsen","suarez","emislsen@hotmail.com");
 
 console.log(madre);
+
