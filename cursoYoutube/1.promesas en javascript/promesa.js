27680eaa123a9dc2b6f0d12a5d4384fe942c6a96
BRANDON_GARCIA <159663607+BrandonGS22b@users.noreply.github.com>
2024-06-17 01:57:11 -0500

    callbacks

---
+++ cursoYoutube/1.promesas en javascript/promesa.js
@@ -0,0 +1,12 @@
+//las promesas son estructuras que nosotros vamosa a poder definir para poderle decir al navegador que queremos que espere a que nosotros terminemos de hacer una operacion
+
+//y que cuado termine de hacer la operacion nosotros le respondemos con los datos
+
+
+
+//vamos a definir nuestras promesas
+
+const promesa = new Promise(); {
+
+
+};
